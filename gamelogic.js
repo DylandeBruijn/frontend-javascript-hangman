caf58fa4ffa4fbca4f21131efe08a45fca665013
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-04-26 20:28:23 +0200

    Uitwerking gamelogic.js

--- gamelogic.js
+++ gamelogic.js
@@ -1,13 +1,46 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+  let displayedWord = "";
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    const isGuessed = guesses.includes(letter);
+    if (isGuessed) {
+      const toAdd = `${letter} `;
+      displayedWord += toAdd;
+    } else {
+      displayedWord += "_ ";
+    }
+  }
+  return displayedWord;
 }
 
 function isGameWon(word, guesses) {
-  // WRITE ME
+  let gameWon = false;
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    const isGuessed = guesses.includes(letter);
+    if (!isGuessed) {
+      gameWon = false;
+    } else {
+      gameWon = true;
+    }
+  }
+  return gameWon;
 }
 
 function isGameLost(word, guesses) {
-  // WRITE ME
+  let wrongLetterCounter = 0;
+  for (let i = 0; i < guesses.length; i++) {
+    const guess = guesses[i];
+    const isCharacter = word.includes(guess);
+    if (!isCharacter) {
+      wrongLetterCounter++;
+    }
+  }
+  if (wrongLetterCounter >= 7) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
